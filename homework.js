58346e2e5c43b944c2de112aca6a86ae511a897b
Gideon Nti Boateng <gideon.boateng@amalitech.org>
2021-09-16 15:24:50 -0800

    Initial commit

---
+++ homework.js
@@ -0,0 +1,29 @@
+// Mary Rose's JavaScript Homework
+
+// 1. Write an if/else statement
+var APPLE_QUANTITY = 5;
+
+if (APPLE_QUANTITY < 5){
+  console.log("You have fewer than 5 apples.");
+} else {
+  console.log("You have 5 or more apples. Hurrah!");
+}
+//You are correct for this part
+
+// 2. Write a "for" loop
+for(i = 0; i < APPLE_QUANTITY; i ++){
+  console.log("WEEEEEEE! I'm looping!")
+}
+//Kindly work on your puntuations
+// 3. Write a function
+var sayHello = function(){
+  console.log("Hello!")
+}
+//next time use let instaed of var
+// 4. Create an object
+var mary = {
+  name: "Mary Rose",
+  codeNinja: true,
+  country: "UK"
+}
+//Nice work
